9052d3a5de6cd5c59539f5cd8738918ea33e6db3
Bryson <brysonpdavis@github.com>
2021-01-08 03:59:07 -0500

    test 5

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,45 +1,53 @@
 import React, {useState} from 'react'
 
 const UploadForm = () => {
-    const [name, setName] = useState('')
+    const [_name, setName] = useState('')
     // const [audioFile, setAudioFile] = useState({})
     const [description, setDescription] = useState('')
     const [contributor, setContributor] = useState('')
 
-    // const encode = (data) => {
-    //     return Object.keys(data)
-    //         .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    //         .join("&")
-    // }
+    const encode = (data) => {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&")
+    }
     
-    // // {
-    // //     const formData = new FormData()
-    // //     Object.keys(data).forEach(k=>{
-    // //         formData.append(k, data[k])
-    // //     })
-    // //     return formData
-    // // }
-
-    // const handleSubmit = event => {
-    //     if (true) {
-    //         const data = {name, description, contributor, "file" : audioFile}
-            
-    //         //send to netlify client, which forwards to aws then to fauna?
-
-    //         fetch("/", {
-    //             method: "POST",
-    //             headers: {"Content-Type": "application/x-www-form-urlencoded"},
-    //             body: encode(data)
-    //         })
-    //         .then(() => console.log("Submission successful"))
-    //         .catch(error => console.log("Submission failed", error))
-    //     }  
-    //     event.preventDefault()
+    // {
+    //     const formData = new FormData()
+    //     Object.keys(data).forEach(k=>{
+    //         formData.append(k, data[k])
+    //     })
+    //     return formData
     // }
+
+    const handleSubmit = event => {
+        console.log(event)
+        if (true) {
+            const data = {
+                "form-name": "upload", 
+                "_name": _name, 
+                "contributor": contributor, 
+                "description": description, 
+                // "_file" : audioFile
+            }
+            
+            //send to netlify client, which forwards to aws then to fauna?
+
+            fetch("/", {
+                method: "POST",
+                headers: {"Content-Type": "application/x-www-form-urlencoded"},
+                body: encode(data)
+            })
+            .then(() => console.log("Submission successful"))
+            .catch(error => console.log("Submission failed", error))
+        }  
+
+        event.preventDefault()
+    }
 
     const handleChange = e => {
         const {name, value} = e.target
-        if (name === 'name'){
+        if (name === '_name'){
             return setName(value)
         }
         if (name === 'description'){
@@ -48,17 +56,17 @@ const UploadForm = () => {
         if (name === 'contributor'){
             return setContributor(value)
         }
-        // if (name === 'file'){
+        // if (name === '_file'){
         //     return setAudioFile(value)
         // }
     }
 
     return (
-        <form name="upload" action="POST">
+        <form name="upload" method="POST" onSubmit={handleSubmit}>
             <input type="hidden" name="form-name" value="upload" />
             <p>
                 <label>
-                    sample name : <input type="text" name="name" value={name} onChange={handleChange} />
+                    sample name : <input type="text" name="_name" value={_name} onChange={handleChange} />
                 </label>
             </p>
             <label>
@@ -71,11 +79,11 @@ const UploadForm = () => {
                     description : <input type="text" name="description" value={description} onChange={handleChange} />
                 </p>
             </label>
-            <label>
+            {/* <label>
                 <p>
-                    file : <input type="file" name="file" className="custom-file-input" onChange={handleChange}></input>
+                    file : <input type="file" name="_file" className="custom-file-input" onChange={handleChange}></input>
                 </p>
-            </label>
+            </label> */}
             <button type="submit">upload</button>
         </form>
     )
