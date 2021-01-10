fdb6d9ba3e0516902cc8df2b9f1a6a653723af0c
Bryson <brysonpdavis@github.com>
2021-01-10 14:29:59 -0500

    added 'submission-added' function

---
+++ functions/submission-created.js
@@ -0,0 +1,15 @@
+const AWS = require("aws-sdk")
+
+exports.handler = async (event, context) => {
+    // const s3 = new AWS.S3({
+    //     accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+    //     secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
+    // })
+
+    // uploadParams = {Bucket: 'grainstems', Key: '', Body: ''}
+    // file = event.
+
+    console.log(event.body)
+}
+
+
